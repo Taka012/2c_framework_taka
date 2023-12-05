be6cde95c90ea0d51dc00012538fd5c588c987ab
S0532 <S0532@19S0532>
2023-12-05 14:50:43 +0900

    なかの12/05

--- Sotusei/src/main/java/com/example/demo/controller/HiphopController.java
+++ Sotusei/src/main/java/com/example/demo/controller/HiphopController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HiphopController {
-
+ 
     @GetMapping("/hiphop")
     public String showKeijibanPage() {
         return "hiphop";
